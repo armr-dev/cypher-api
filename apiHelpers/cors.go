ee8e535c606bc2aa7001426edab9de35eb9d65a3
André Ricardo <andrermr.dev@gmail.com>
2021-07-18 11:55:27 -0400

    :recycle::truck: move setupCorsResponse

---
+++ apiHelpers/cors.go
@@ -0,0 +1,11 @@
+package apiHelpers
+
+import (
+	"net/http"
+)
+
+func setupCorsResponse(w *http.ResponseWriter, r *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
